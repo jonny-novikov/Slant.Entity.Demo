b725daab583be9499b41c35853e5fa2bcf1f64bf
Ivan Novikov <jonny.novikov@gmail.com>
2023-09-21 21:26:25 +0300

    ValidationState refactoring

--- Validation/ValidationResult.cs
+++ Validation/ValidationResult.cs
@@ -5,17 +5,30 @@ using System.Linq;
 
 namespace Slant.Entity.Demo.Validation;
 
-public enum ErrorType
+/// <summary>
+/// Specifies the severity of a rule.
+/// </summary>
+public enum Severity 
 {
-    Blocking,
-    Warning
+    /// <summary>
+    /// Ошибка
+    /// </summary>
+    Error,
+    /// <summary>
+    /// Предупреждение
+    /// </summary>
+    Warning,
+    /// <summary>
+    /// Информация
+    /// </summary>
+    Info
 }
 
-public record ValidationError(ErrorType ErrorType, string Field, string Message);
+public abstract record ValidationState(Severity Severity, string Field, string Message);
 
-public record BlockingError(string Field, string Message) : ValidationError(ErrorType.Blocking, Field, Message);
+public record ValidationError(string Field, string Message) : ValidationState(Severity.Error, Field, Message);
 
-public record ValidationWarning(string Field, string Message) : ValidationError(ErrorType.Warning, Field, Message);
+public record ValidationWarning(string Field, string Message) : ValidationState(Severity.Warning, Field, Message);
 
 /*
 public class ValidationResult
@@ -32,9 +45,9 @@ public interface IValidationState<T>
 {
     bool IsValid { get; }
         
-    List<ValidationError> Errors { get; }
-        
-    List<ValidationError> Warnings { get; }
+    List<ValidationState> Errors { get; }
+
+    List<ValidationState> Warnings { get; }
 }
 
 public class UserValidationService
@@ -48,32 +61,32 @@ public class UserValidationService
     sealed class NotValidated<T> : IValidationState<T>
     {
         public bool IsValid => false;
-        public List<ValidationError> Errors { get; } = new();
-        public List<ValidationError> Warnings { get; } = new();
+        public List<ValidationState> Errors { get; } = new();
+        public List<ValidationState> Warnings { get; } = new();
     }
     
     internal class Validated<T> : IValidationState<T>
     {
-        public Validated(IEnumerable<ValidationError> result)
+        public Validated(IEnumerable<ValidationState> result)
         {
-            ProcessErrors(result);    
+            ProcessResult(result);    
         }
         
-        public Validated(IEnumerable<ValidationError>[] results)
+        public Validated(params IEnumerable<ValidationState>[] results)
         {
-            ProcessManyErrors(results);    
+            ProcessManyResults(results);
         }
 
-        private void ProcessErrors(IEnumerable<ValidationError> result)
+        private void ProcessResult(IEnumerable<ValidationState> result)
         {
             foreach (var error in result)
             {
-                switch (error.ErrorType)
+                switch (error.Severity)
                 {
-                    case ErrorType.Blocking:
+                    case Severity.Error:
                         Errors.Add(error);
                         break;
-                    case ErrorType.Warning:
+                    case Severity.Warning:
                         Warnings.Add(error);
                         break;
                     default:
@@ -84,19 +97,19 @@ public class UserValidationService
             IsValid = Errors.Count == 0;
         }
 
-        private void ProcessManyErrors(params IEnumerable<ValidationError>[] rules)
+        private void ProcessManyResults(params IEnumerable<ValidationState>[] rules)
         {
             foreach (var result in rules)
             {
-                ProcessErrors(result);
+                ProcessResult(result);
             }
         }
 
         public bool IsValid { get; private set; } = true;
 
-        public List<ValidationError> Errors { get; } = new();
+        public List<ValidationState> Errors { get; } = new();
 
-        public List<ValidationError> Warnings { get; } = new();
+        public List<ValidationState> Warnings { get; } = new();
     }
 
     sealed class UserValidator
@@ -109,14 +122,15 @@ public class UserValidationService
                 ValidateEmail(user),
                 ValidateCreditScore(user)
             };
+            // return new Validated<User>(ValidateUserName(user), ValidateEmail(user));
             return new Validated<User>(validationRules);
         }
         
-        private IEnumerable<ValidationError> ValidateUserName(User user)
+        private IEnumerable<ValidationState> ValidateUserName(User user)
         {
             if (string.IsNullOrWhiteSpace(user.Name))
             {
-                yield return new BlockingError(nameof(User.Name), "Username is required");
+                yield return new ValidationError(nameof(User.Name), "Username is required");
             }
             else if (user.Name.Length < 3)
             {
@@ -124,19 +138,19 @@ public class UserValidationService
             }
         }
 
-        private IEnumerable<ValidationError> ValidateEmail(User user)
+        private IEnumerable<ValidationState> ValidateEmail(User user)
         {
             if (string.IsNullOrWhiteSpace(user.Email) || !user.Email.Contains("@"))
             {
-                yield return new ValidationError(ErrorType.Blocking, nameof(User.Email), "A valid email is required");
+                yield return new ValidationError(nameof(User.Email), "A valid email is required");
             }
         }
 
-        private IEnumerable<ValidationError> ValidateCreditScore(User user)
+        private IEnumerable<ValidationState> ValidateCreditScore(User user)
         {
             if (user.CreditScore < 0)
             {
-                yield return new BlockingError(nameof(user.CreditScore),"CreditScore is invalid");
+                yield return new ValidationError(nameof(user.CreditScore),"CreditScore is invalid");
             }
             else if (user.CreditScore == 0)
             {
